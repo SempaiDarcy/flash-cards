4dc48006d0ab7d669d57db69fd956cf1b4a47655
SempaiDarcy <mr_darcy_1@mail.ru>
2023-11-20 01:18:03 +0500

    fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
-import '@/app/styles/index.scss'
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
