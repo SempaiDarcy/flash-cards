c1ec853aa6d8c58b743091bc449485df3869dfb3
SempaiDarcy <mr_darcy_1@mail.ru>
2023-11-19 08:33:52 +0500

    Feat: add styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,17 @@
 import type { Preview } from '@storybook/react'
 
+import './styles/index.scss'
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
