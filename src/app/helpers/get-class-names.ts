43cb7a3d0c566eae58d512ad99488a4dcb2ba1c4
ArtyomSamsonchik <colpachek@gmail.com>
2023-11-24 07:04:05 +0300

    feat: add capitalize and getClassNames helpers

---
+++ src/app/helpers/get-class-names.ts
@@ -0,0 +1,67 @@
+import { capitalize } from '@/app/helpers/capitalize'
+import { clsx } from 'clsx'
+
+type SlotsMap<T extends string> = Record<T, string>
+type ModifiersMap<T extends string> = Record<T, any>
+type NotExisting = null | undefined
+
+// prettier-ignore
+export type ClassesObj<
+  Slot extends string, Modifier extends string = never
+> = [Modifier] extends [never]
+  ? Partial<SlotsMap<Slot>>
+  : Partial<SlotsMap<`${Slot}${Capitalize<Modifier>}`> & SlotsMap<Slot>>
+
+const configure = <Slot extends string, Option extends string = never>(
+  slotNames: Slot[],
+  modifiers?: ModifiersMap<Option>
+) => {
+  return (...classes: (ClassesObj<Slot, Option> | NotExisting)[]) => {
+    const slotsMapArray = classes
+      .map(classesObj => classesObj && getSlotsMap(slotNames, classesObj, modifiers))
+      .filter(Boolean) as SlotsMap<Slot>[]
+
+    return slotsMapArray.length === 1 ? slotsMapArray[0] : mergeSlotsMaps(slotNames, slotsMapArray)
+  }
+}
+
+const mergeSlotsMaps = <Slot extends string>(
+  slotNames: Slot[],
+  slotsMapArray: SlotsMap<Slot>[]
+): SlotsMap<Slot> =>
+  slotNames.reduce((acc, slot) => {
+    return {
+      ...acc,
+      [slot]: slotsMapArray.map(slotsMap => slotsMap[slot]).join(' '),
+    }
+  }, {} as SlotsMap<Slot>)
+
+const getSlotsMap = <Slot extends string, Modifier extends string>(
+  slotNames: Slot[],
+  classes: ClassesObj<Slot, Modifier>,
+  modifiers?: ModifiersMap<Modifier>
+): SlotsMap<Slot> => {
+  return slotNames.reduce(
+    (acc, slot) => ({
+      ...acc,
+      [slot]: getClassNamesForSlot(slot, classes, modifiers),
+    }),
+    {} as SlotsMap<Slot>
+  )
+}
+
+const getClassNamesForSlot = (
+  slot: string,
+  classes: ClassesObj<string, string>,
+  modifiers?: ModifiersMap<string>
+) => {
+  const classNamesWithModifiers = modifiers
+    ? Object.entries(modifiers).map(
+        ([option, value]) => !!value && classes[`${slot}${capitalize(option)}`]
+      )
+    : []
+
+  return clsx(classes[slot], classNamesWithModifiers)
+}
+
+export default configure
