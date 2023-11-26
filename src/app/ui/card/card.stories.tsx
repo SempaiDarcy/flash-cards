0a555b0143339f6658aba22a60f65721313206ec
ArtyomSamsonchik <colpachek@gmail.com>
2023-11-26 12:12:09 +0300

    refactor: make Card component polymorphic
    Improve Card stories description

--- src/app/ui/card/card.stories.tsx
+++ src/app/ui/card/card.stories.tsx
@@ -3,15 +3,25 @@ import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   argTypes: {
+    as: {
+      description: `Must be a string representing a React common component (such as a  div).
+        If prop is not specified, it is set to 'div'.`,
+      table: { defaultValue: { summary: 'div' }, type: { summary: 'ElementType' } },
+      type: 'string',
+    },
+
     className: {
       description: 'String merged with the default root element class name',
       table: { type: { summary: 'string' } },
+      type: 'string',
     },
+
     ref: {
       description: 'A ref, forwarded to the root slot of the card component',
-      table: { type: { summary: 'Ref<HTMLDivElement' } },
+      table: { type: { summary: 'Ref<ElementRef<T>>, T extends ElementType' } },
     },
   },
+
   component: Card,
   tags: ['autodocs'],
   title: 'Components/Card',
