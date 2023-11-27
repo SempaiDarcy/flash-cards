79c830c2ff45afd1caaf41f8963686ac27cbaaef
SempaiDarcy <mr_darcy_1@mail.ru>
2023-11-28 00:33:36 +0500

    feat:added sign in form and styles and stories

---
+++ src/app/ui/sign-in-form/sign-in-form.stories.ts
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { SignInForm } from './sign-in-form'
+
+const meta = {
+  component: SignInForm,
+  tags: ['autodocs'],
+  argTypes: {
+    onSubmit: {
+      action: 'Signed In',
+    },
+  },
+} satisfies Meta<typeof SignInForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
